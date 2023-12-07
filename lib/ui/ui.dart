830ec08c867cde81ee31fed4f118c2532ad6ef65
Pholluxion <penalozadaniel00@gmail.com>
2023-12-06 20:33:18 -0500

    06/12/2023: Update

--- lib/ui/ui.dart
+++ lib/ui/ui.dart
@@ -2,5 +2,3 @@ export 'extension/extension.dart';
 export 'widgets/widgets.dart';
 export 'spacing/spacing.dart';
 export 'colors/colors.dart';
-export 'gen/assets.gen.dart';
-export 'gen/fonts.gen.dart';
